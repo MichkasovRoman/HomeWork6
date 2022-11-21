c9f015a26eacc94ae4763318dbeecc258966baf0
Michkasov Roman <mihjob@list.ru>
2022-11-21 23:55:32 +0300

    changed task50 a little bit

--- task50/Program.cs
+++ task50/Program.cs
@@ -5,22 +5,23 @@
 //(номер строки и столбца) этого элемента 
 //или же указание, что такого элемента нет.
 
-double[,] GetArray(int row, int column, double minValue, double maxValue)
+int[,] GetArray(int row, int column, int minValue, int maxValue)
 {
-    double[,] result = new double[row, column];
+    int[,] result = new int[row, column];
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < column; j++)
         {
-            result[i, j] = Math.Round(new Random().NextDouble() * (maxValue - minValue) + minValue, 2);
+            result[i, j] = new Random().Next(minValue, maxValue);
         }
     }
     return result;
 }
 
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
     Console.WriteLine("Двумерный массив: ");
+    Console.WriteLine(String.Empty);
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
@@ -31,12 +32,12 @@ void PrintArray(double[,] array)
     }
 }
 
-void SearchForElement(double[,] array)
+void SearchForElement(int[,] array)
 {
     Console.Write("Введите искомый элемент массива: ");
-    double desiredElement = double.Parse(Console.ReadLine()!);
-    double index1 = 0;
-    double index2 = 0;
+    int desiredElement = int.Parse(Console.ReadLine()!);
+    int index1 = -1;
+    int index2 = -1;
     for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
@@ -48,8 +49,16 @@ void SearchForElement(double[,] array)
                         index2 = j;
                     } 
                 }
-        }  
-    Console.Write($"Элемент {desiredElement} находится в {index1}-й строке и в {index2}-м столбце."); 
+        }
+    if (index1 == -1 && index2 == -1)
+    {
+        Console.WriteLine("Введенного вами элемента нет в массиве.");
+        Console.WriteLine("Перезапустите программу и введите число заново.");
+    }
+    else
+    {
+        Console.Write($"Элемент {desiredElement} находится в {index1}-й строке и в {index2}-м столбце."); 
+    }
 }
 
 Console.Clear();
@@ -59,13 +68,14 @@ int m = int.Parse(Console.ReadLine()!);
 Console.Write("Задайте количество столбцов n = ");
 int n = int.Parse(Console.ReadLine()!);
 Console.Write("Введите нижнюю границу диапазона элементов массива: ");
-double lower = double.Parse(Console.ReadLine()!);
+int lower = int.Parse(Console.ReadLine()!);
 Console.Write("Введите верхнюю границу диапазона элементов массива: ");
-double upper = double.Parse(Console.ReadLine()!);
+int upper = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine(" ");
 
-double[,] workingArray = GetArray(m, n, lower, upper);
+int[,] workingArray = GetArray(m, n, lower, upper);
 PrintArray(workingArray);
 Console.WriteLine(" ");
 SearchForElement(workingArray);
+
